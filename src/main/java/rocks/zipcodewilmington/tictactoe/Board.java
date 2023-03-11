52bc9f4830233fcd9ca641bbfb5df2e0dac8e8d9
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-11 16:20:23 -0500

    passed all tests

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,44 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
-    }
+    public static Character[][] matrix;
+        public Board (Character[][] matrix) {
+            this.matrix = matrix;
+        }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return (horizontalCheck('X') || verticalCheck('X') || diagonalCheck('X'));
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return (horizontalCheck('O') || verticalCheck('O') || diagonalCheck('O'));
     }
 
     public Boolean isTie() {
-        return null;
+        return ((!isInFavorOfO())) && (!isInFavorOfX());
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfO()){
+            return "O";
+        } else if (isInFavorOfX()) {
+            return "X";
+        } else {
+            return "";}
     }
 
+public boolean diagonalCheck(char play) {
+    return ((play == matrix [0][0] && play == matrix [1][1] && play == matrix [2][2])
+            || (play == matrix [0][2] && play == matrix [1][1] && play == matrix [2][0]));
+}
+    public boolean horizontalCheck(char play) {
+        return ((play == matrix [0][0] && play == matrix [0][1] && play == matrix [0][2])
+                || (play == matrix [1][0] && play == matrix [1][1] && play == matrix [1][2]))
+                || (play == matrix [2][0] && play == matrix [2][1] && play == matrix [2][2]);
+    }
+    public boolean verticalCheck(char play) {
+        return ((play == matrix [0][0] && play == matrix [1][0] && play == matrix [2][0])
+                || (play == matrix [0][1] && play == matrix [1][1] && play == matrix [2][1]))
+                || (play == matrix [0][2] && play == matrix [1][2] && play == matrix [2][2]);
+    }
 }
